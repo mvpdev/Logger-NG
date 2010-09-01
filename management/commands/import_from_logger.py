ddd29a6ad002c5a178404250a1d1252360b372c1
ksamuel <info.ksamuel@gmail.com>
2010-09-01 23:42:28 +0200

    Import script have been ported

--- management/commands/import_from_logger.py
+++ management/commands/import_from_logger.py
@@ -6,7 +6,7 @@
 A helper command to export your old logger records to the new logger_ng.
 
 To run, simply replace logger in your local.ini apps list with logger_ng, then
-run: ./rapidsms import_from_logger
+run: ./manage.py import_from_logger
 
 It will even (somewhat) intelligently associate outgoing messages with their
 corresponding incoming message, based on the message identity, backend, and
@@ -18,36 +18,32 @@ In other words, it won't let you accidentally import your old logs twice.
 '''
 
 import random
+import sys
 from datetime import timedelta
 
 from django.utils.translation import ugettext as _
 from django.core.management.base import BaseCommand, CommandError
 
-from logger.models import IncomingMessage, OutgoingMessage
+from rapidsms.contrib.messagelog.models import Message
 from logger_ng.models import LoggedMessage
-from reporters.models import PersistantConnection
+from rapidsms.models import Connection
 
 
 def create_from_logger_msg(msg):
     '''
-    Helper function that takes either an IncomingMessage or Outgoing
-    message (from the old logger app) and creates a LoggedMessage
+    Helper function that takes either a Message
+     (from the old logger app) and creates a LoggedMessage
     object from it. It saves the new LoggedMessage then returns it.
     '''
-    if isinstance(msg, IncomingMessage):
-        direction = LoggedMessage.DIRECTION_INCOMING
-    else:
-        direction = LoggedMessage.DIRECTION_OUTGOING
 
-    try:
-        reporter = PersistantConnection \
-                            .objects.get(backend__slug=msg.backend,
-                                         identity=msg.identity).reporter
-    except PersistantConnection.DoesNotExist:
-        reporter = None
-    msg_lng = LoggedMessage(identity=msg.identity, backend=msg.backend,
-                            text=msg.text, reporter=reporter,
-                            direction=direction)
+    if not msg.connection:
+        print "\nThe message %s doesn't have a connection. Skipped." % msg
+        return None
+
+    msg_lng = LoggedMessage(identity=msg.connection.identity, 
+                            backend=msg.connection.backend.name,
+                            text=msg.text, contact=msg.contact, 
+                            direction=msg.direction)
     msg_lng.save()
     msg_lng.date = msg.date
     msg_lng.save()
@@ -56,13 +52,13 @@ def create_from_logger_msg(msg):
 
 class Command(BaseCommand):
     '''
-    This class _must_ be named command subclass BaseCommand to work.
+    This class _must_ be named command and subclass BaseCommand to work.
     '''
 
     def handle(self, *args, **options):
         '''
         Do the import, will be called when the management call of
-            ./rapidsms import_from_logger
+            ./manage.py import_from_logger
         is called.
         '''
 
@@ -78,8 +74,7 @@ class Command(BaseCommand):
         OUTGOING = LoggedMessage.DIRECTION_OUTGOING
         INCOMING = LoggedMessage.DIRECTION_INCOMING
 
-        if not IncomingMessage.objects.count() and \
-           not OutgoingMessage.objects.count():
+        if not Message.objects.count():
             raise CommandError(_(u"There are no messages in the logger app " \
                                  u"to import."))
 
@@ -88,49 +83,53 @@ class Command(BaseCommand):
         # already exist in logger_ng, we exit without importing.
         checks = []
         for i in range(0, 5):
-            if IncomingMessage.objects.count():
-                checks.append(random.choice(IncomingMessage.objects.all()))
-            if OutgoingMessage.objects.count():
-                checks.append(random.choice(OutgoingMessage.objects.all()))
+            checks.append(random.choice(Message.objects.all()))
 
         for msg in checks:
-            messages = LoggedMessage.objects.filter(identity=msg.identity,
-                                                    backend=msg.backend,
-                                                    text=msg.text,
-                                                    date=msg.date)
-            if (isinstance(msg, IncomingMessage) and \
-               messages.filter(direction=INCOMING).count()) or \
-               (isinstance(msg, OutgoingMessage) and \
-               messages.filter(direction=OUTGOING).count()):
-                raise CommandError(_(u"It appears that you have already " \
-                                     u"imported your messages."))
+            if msg.connection:
+                messages = LoggedMessage.objects.filter(identity=msg.connection.identity,
+                                                        backend=msg.connection.backend.name,
+                                                        text=msg.text,
+                                                        date=msg.date)
+                                                    
+                if messages.filter(direction=msg.direction).count():
+                    raise CommandError(_(u"It appears that you have already " \
+                                         u"imported your messages."))
+            else:
+                print _(u"The message %s doesn't have a connection." % msg)
 
         print _(u"Importing %d incoming messages...") % \
-              IncomingMessage.objects.count()
-        for msg in IncomingMessage.objects.all():
+              Message.objects.filter(direction=INCOMING).count()
+        for msg in Message.objects.filter(direction=INCOMING):
             msg_lng = create_from_logger_msg(msg)
-            msg_lng.save()
+            if msg_lng: 
+                msg_lng.save()
 
         print _(u"Importing %d outgoing messages...") % \
-              OutgoingMessage.objects.count()
+              Message.objects.filter(direction=OUTGOING).count()
+              
         count = 0
-        for msg in OutgoingMessage.objects.all():
+        for msg in Message.objects.filter(direction=OUTGOING):
+            sys.stdout.write('.')
             msg_lng = create_from_logger_msg(msg)
-            if SECONDS_BEFORE_MATCH > 0:
-                just_before = msg.date - \
-                              timedelta(seconds=SECONDS_BEFORE_MATCH)
-                try:
-                    orig = LoggedMessage.incoming.get(identity=msg.identity,
-                                                      backend=msg.backend,
-                                                      date__gte=just_before,
-                                                      date__lte=msg.date)
-                except (LoggedMessage.DoesNotExist,
-                        LoggedMessage.MultipleObjectsReturned):
-                    pass
-                else:
-                    count += 1
-                    msg_lng.response_to = orig
-            msg_lng.save()
+            
+            if msg_lng:
+                if SECONDS_BEFORE_MATCH > 0:
+                    just_before = msg.date - timedelta(seconds=SECONDS_BEFORE_MATCH)
+                    try:
+                        orig = LoggedMessage.incoming.get(identity=msg.connection.identity,
+                                                          backend=msg.connection.backend.name,
+                                                          date__gte=just_before,
+                                                          date__lte=msg.date)
+                    except (LoggedMessage.DoesNotExist,
+                            LoggedMessage.MultipleObjectsReturned):
+                        pass
+                    else:
+                        count += 1
+                        msg_lng.response_to = orig
+                        
+                msg_lng.save()
+        print
 
         print _(u"%d outgoing messages paired with " \
                 u"their incoming messages.") % count
